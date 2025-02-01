df82e573f6028267b7acb4e55dc586d03b87526f
adrian daniel gutierrez martinez <adriand@estudiantes.uci.cu>
2025-02-01 22:14:56 +0100

    Eliminación de React Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router";
 import AppRouter from "@/router/AppRouter";
 // import { HashRouter } from "react-router";
-
-const queryClient = new QueryClient();
 
 export function App() {
   return (
     // <HashRouter> 
     <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <AppRouter/>
-      </QueryClientProvider>
+      <AppRouter/>
     </BrowserRouter>
     // </HashRouter>
   )
